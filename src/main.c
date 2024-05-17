c52df84eccebc5797940e480840a041d477fd5fa
foldedtoad <foldedtoad@gmail.com>
2024-05-16 17:49:47 -0700

    demo now working for xiao

--- src/main.c
+++ src/main.c
@@ -16,6 +16,10 @@
 #include <zephyr/sys/poweroff.h>
 #include <zephyr/sys/util.h>
 
+#define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
+#include <zephyr/logging/log.h>
+LOG_MODULE_REGISTER(sys_off);
+
 static const struct gpio_dt_spec sw0 = GPIO_DT_SPEC_GET(DT_ALIAS(sw0), gpios);
 
 int main(void)
@@ -24,11 +28,11 @@ int main(void)
 	const struct device *const cons = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 	if (!device_is_ready(cons)) {
-		printf("%s: device not ready.\n", cons->name);
+		LOG_ERR("%s: device not ready.", cons->name);
 		return 0;
 	}
 
-	printf("\n%s system off demo\n", CONFIG_BOARD);
+	LOG_INF("%s system-off demo", CONFIG_BOARD);
 
 	if (IS_ENABLED(CONFIG_APP_RETENTION)) {
 		bool retained_ok = retained_validate();
@@ -37,32 +41,32 @@ int main(void)
 		retained.boots += 1;
 		retained_update();
 
-		printf("Retained data: %s\n", retained_ok ? "valid" : "INVALID");
-		printf("Boot count: %u\n", retained.boots);
-		printf("Off count: %u\n", retained.off_count);
-		printf("Active Ticks: %" PRIu64 "\n", retained.uptime_sum);
+		LOG_INF("Retained data: %s", retained_ok ? "valid" : "INVALID");
+		LOG_INF("Boot count: %u", retained.boots);
+		LOG_INF("Off count: %u", retained.off_count);
+		LOG_INF("Active Ticks: %" PRIu64, retained.uptime_sum);
 	} else {
-		printf("Retained data not supported\n");
+		LOG_INF("Retained data not supported");
 	}
 
 	/* configure sw0 as input, interrupt as level active to allow wake-up */
 	rc = gpio_pin_configure_dt(&sw0, GPIO_INPUT);
 	if (rc < 0) {
-		printf("Could not configure sw0 GPIO (%d)\n", rc);
+		LOG_ERR("Could not configure sw0 GPIO (%d)", rc);
 		return 0;
 	}
 
 	rc = gpio_pin_interrupt_configure_dt(&sw0, GPIO_INT_LEVEL_ACTIVE);
 	if (rc < 0) {
-		printf("Could not configure sw0 GPIO interrupt (%d)\n", rc);
+		LOG_ERR("Could not configure sw0 GPIO interrupt (%d)", rc);
 		return 0;
 	}
 
-	printf("Entering system off; press sw0 to restart\n");
+	LOG_INF("Entering system off; press sw0 to restart");
 
 	rc = pm_device_action_run(cons, PM_DEVICE_ACTION_SUSPEND);
 	if (rc < 0) {
-		printf("Could not suspend console (%d)\n", rc);
+		LOG_ERR("Could not suspend console (%d)", rc);
 		return 0;
 	}
 
